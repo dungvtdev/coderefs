f42cd72d997227738a3d5b452e7b920b551aad42
dungvt <dungvt.dev@gmail.com>
2017-01-20 23:58:06 +0700

    temp add register

--- django_angular/django-angular-example/django_angular_example/urls.py
+++ django_angular/django-angular-example/django_angular_example/urls.py
@@ -1,15 +1,17 @@
 # .. Imports
-from rest_framework_nested import routers
+from django.conf.urls import url, include
+
+from rest_framework import routers
 
 from authentication.views import AccountViewSet
+
+from .views import IndexView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 
-urlpatterns = patterns(
-     '',
-    # ... URLs
+urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
-)
+]
--- django_angular/django-angular-example/static/javascripts/thinkster.js
+++ django_angular/django-angular-example/static/javascripts/thinkster.js
@@ -1,2 +1,26 @@
-angular
-  .module('thinkster', []);
+(function () {
+  angular
+    .module('thinkster', [
+      'thinkster.routes',
+      'thinkster.authentication',
+      'thinkster.config'
+    ]);
+
+  angular
+    .module('thinkster.routes', ['ngRoute']);
+
+  angular
+    .module('thinkster.config', []);
+
+  angular
+    .module('thinkster')
+    .run(run);
+
+  run.$inject = ['$http'];
+
+  function run($http){
+    $http.defaults.xsrfHeaderName = 'x-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  }
+})();
+
